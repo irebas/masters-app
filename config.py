ab0d09139e76d77e1e67e03193a7b3040d4fc774
irebas <igor.rebas@randstad.pl>
2023-05-03 17:14:47 +0200

    test

--- config.py
+++ config.py
@@ -4,7 +4,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     postgres_url = 'postgresql://masters_app_user:AyvTx0G6KY0worz83Uw1UQRYghUJaLBl@dpg-ch955iukobicv5rll8ag-a.frankfurt-postgres.render.com/masters_app'
     sqlite_url = "sqlite:///masters.db"
-    # SQLALCHEMY_DATABASE_URI = postgres_url
-    SQLALCHEMY_DATABASE_URI = sqlite_url
+    if str(os.getenv('HOME')).lower().find('users'):
+        SQLALCHEMY_DATABASE_URI = postgres_url
+        # SQLALCHEMY_DATABASE_URI = sqlite_url
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SECRET_KEY = 'xyz'
     DEBUG = True
